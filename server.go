2f66c1dda74fb7a4a9224e66d3a6628d3bfb5862
liangqi1 <liangqi000@gmail.com>
2020-05-10 18:42:22 +0800

    add ocsp server

--- server.go
+++ server.go
@@ -1,23 +1,34 @@
 package gracehttp
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
+	"crypto/x509"
+	"errors"
 	"fmt"
+	"golang.org/x/crypto/ocsp"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"sync"
 	"syscall"
 	"time"
 )
+
+// 当前OCSP查询内存缓存
+var cache []byte
+var locker sync.RWMutex
 
 const (
 	GRACEFUL_ENVIRON_KEY    = "IS_GRACEFUL"
 	GRACEFUL_ENVIRON_STRING = GRACEFUL_ENVIRON_KEY + "=1"
 	GRACEFUL_LISTENER_FD    = 3
+	OCSP_DURATION           = 10 // 10 minute
 )
 
 // HTTP server that supported graceful shutdown or restart
@@ -29,6 +40,8 @@ type Server struct {
 	isGraceful   bool
 	signalChan   chan os.Signal
 	shutdownChan chan bool
+	certFile     string
+	certKey      string
 }
 
 func NewServer(addr string, handler http.Handler, readTimeout, writeTimeout time.Duration) *Server {
@@ -55,10 +68,10 @@ func NewServer(addr string, handler http.Handler, readTimeout, writeTimeout time
 func (srv *Server) ListenAndServe() error {
 	addr := srv.Addr
 	if addr == "" {
-		addr = ":http"
+		srv.Addr = ":http"
 	}
 
-	ln, err := srv.getNetListener(addr)
+	ln, err := srv.getNetListener()
 	if err != nil {
 		return err
 	}
@@ -67,12 +80,17 @@ func (srv *Server) ListenAndServe() error {
 	return srv.Serve()
 }
 
-func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
+func (srv *Server) initServer(certFile string, keyFile string) {
 	addr := srv.Addr
 	if addr == "" {
-		addr = ":https"
+		srv.Addr = ":https"
 	}
 
+	srv.certFile = certFile
+	srv.certKey = keyFile
+}
+
+func (srv *Server) initConfig() *tls.Config {
 	config := &tls.Config{}
 	if srv.TLSConfig != nil {
 		config = srv.TLSConfig.Clone()
@@ -81,6 +99,47 @@ func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
 	if config.NextProtos == nil {
 		config.NextProtos = []string{"http/1.1"}
 	}
+
+	return config
+}
+
+func (srv *Server) ListenAndServeTLSOcsp(certFile, keyFile string) error {
+	srv.initServer(certFile, keyFile)
+
+	config := srv.initConfig()
+	configHasCert := len(config.Certificates) > 0 || config.GetCertificate != nil
+	if !configHasCert {
+		config.GetCertificate = srv.GetCertificateWithOcsp
+		srv.asyncOcspCache()
+	}
+
+	ln, err := srv.getNetListener()
+	if err != nil {
+		return err
+	}
+
+	srv.listener = tls.NewListener(ln, config)
+	return srv.Serve()
+}
+
+func (srv *Server) GetCertificateWithOcsp(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+	cert, err := tls.LoadX509KeyPair(srv.certFile, srv.certKey)
+	if err != nil {
+		return nil, err
+	}
+
+	if cache != nil {
+		locker.RLock()
+		cert.OCSPStaple = cache
+		locker.RUnlock()
+	}
+
+	return &cert, nil
+}
+
+func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
+	srv.initServer(certFile, keyFile)
+	config := srv.initConfig()
 
 	configHasCert := len(config.Certificates) > 0 || config.GetCertificate != nil
 	if !configHasCert || certFile != "" || keyFile != "" {
@@ -93,7 +152,7 @@ func (srv *Server) ListenAndServeTLS(certFile, keyFile string) error {
 		}
 	}
 
-	ln, err := srv.getNetListener(addr)
+	ln, err := srv.getNetListener()
 	if err != nil {
 		return err
 	}
@@ -113,7 +172,7 @@ func (srv *Server) Serve() error {
 	return err
 }
 
-func (srv *Server) getNetListener(addr string) (net.Listener, error) {
+func (srv *Server) getNetListener() (net.Listener, error) {
 	var ln net.Listener
 	var err error
 
@@ -125,7 +184,7 @@ func (srv *Server) getNetListener(addr string) (net.Listener, error) {
 			return nil, err
 		}
 	} else {
-		ln, err = net.Listen("tcp", addr)
+		ln, err = net.Listen("tcp", srv.Addr)
 		if err != nil {
 			err = fmt.Errorf("net.Listen error: %v", err)
 			return nil, err
@@ -217,5 +276,80 @@ func (srv *Server) logf(format string, args ...interface{}) {
 		srv.ErrorLog.Printf(format, args...)
 	} else {
 		log.Printf(format, args...)
+	}
+}
+
+func (srv *Server) requestOCSP() error {
+	cert, err := tls.LoadX509KeyPair(srv.certFile, srv.certKey)
+	if err != nil {
+		return err
+	}
+
+	if len(cert.Certificate) <= 1 {
+		return errors.New("the cert have no leaf")
+	}
+
+	// 获取leaf证书，第一个证书
+	x509Cert, err := x509.ParseCertificate(cert.Certificate[0])
+	if err != nil {
+		return err
+	}
+
+	ocspServer := x509Cert.OCSPServer[0]
+	if ocspServer == "" {
+		return errors.New("ocsp server is empty")
+	}
+
+	x509Issuer, err := x509.ParseCertificate(cert.Certificate[1])
+	if err != nil {
+		return err
+	}
+
+	ocspRequest, err := ocsp.CreateRequest(x509Cert, x509Issuer, nil)
+	if err != nil {
+		return err
+	}
+
+	ocspRequestReader := bytes.NewReader(ocspRequest)
+	c := &http.Client{
+		Timeout: time.Second * 60,
+	}
+
+	httpResponse, err := c.Post(ocspServer, "application/ocsp-request", ocspRequestReader)
+	if err != nil {
+		return err
+	}
+	defer httpResponse.Body.Close()
+
+	if httpResponse.StatusCode != http.StatusOK {
+		return errors.New(fmt.Sprintf("csp rsp code not 200: %s", httpResponse.Status))
+	}
+
+	ocspRsp, err := ioutil.ReadAll(httpResponse.Body)
+	if err != nil {
+		return err
+	}
+
+	locker.Lock()
+	cache = ocspRsp
+	locker.Unlock()
+
+	return nil
+}
+
+func (srv *Server) asyncOcspCache()  {
+	go srv.scheduleOcsp()
+}
+
+func (srv *Server) scheduleOcsp() {
+	t := time.NewTicker(time.Minute * OCSP_DURATION)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-t.C:
+			// TODO log
+			_ = srv.requestOCSP()
+		}
 	}
 }
